830a2a253216a7a8baa401dc86e2f64a94b4267c
matthewzhang <mz8189@princeton.edu>
2023-10-30 02:07:14 -0400

    edits

--- symtable.h
+++ symtable.h
@@ -3,37 +3,39 @@
 
 #include <stdlib.h>
 
-/* SymTable_T is a pointer to a SymTable object. */
+/* Opaque pointer to a symbol table. */
 typedef struct SymTable *SymTable_T;
 
-/* SymTable_new() creates a new empty SymTable object and returns a pointer to it.
- * Returns NULL if memory allocation failed. */
+/* Function to create a new symbol table. */
 SymTable_T SymTable_new(void);
 
-/* SymTable_free() frees all memory allocated for oSymTable.
- * Does nothing if oSymTable is NULL. */
+/* Function to free all memory occupied by the symbol table. */
 void SymTable_free(SymTable_T oSymTable);
 
-/* SymTable_getLength() returns the number of bindings in oSymTable. */
+/* Function to get the number of bindings in the symbol table. */
 size_t SymTable_getLength(SymTable_T oSymTable);
 
-/* SymTable_put() inserts a new binding in oSymTable.
- * Returns 1 if a new binding was inserted, 0 if a binding with the same key
- * already existed, and -1 if memory allocation failed. */
-int SymTable_put(SymTable_T oSymTable,
-    const char *pcKey, const void *pvValue);
+/* Function to insert a new binding into the symbol table.
+   Returns 1 (true) if the binding was added, and 0 (false) if the key is already in the table. */
+int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue);
 
-void *SymTable_replace(SymTable_T oSymTable,
-    const char *pcKey, const void *pvValue);
+/* Function to replace the value of an existing binding in the symbol table.
+   Returns the old value if the binding exists, and NULL otherwise. */
+void *SymTable_replace(SymTable_T oSymTable, const char *pcKey, const void *pvValue);
 
+/* Function to check if a binding with the given key exists in the symbol table.
+   Returns 1 (true) if the binding exists, and 0 (false) otherwise. */
 int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
 
+/* Function to get the value of a binding in the symbol table.
+   Returns the value if the binding exists, and NULL otherwise. */
 void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
 
+/* Function to remove a binding from the symbol table.
+   Returns the value of the removed binding if it exists, and NULL otherwise. */
 void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
 
-void SymTable_map(SymTable_T oSymTable,
-    void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
-    const void *pvExtra);
+/* Function to apply a function to all bindings in the symbol table. */
+void SymTable_map(SymTable_T oSymTable, void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra), const void *pvExtra);
 
-#endif
+#endif /* SYMTABLE_H */
